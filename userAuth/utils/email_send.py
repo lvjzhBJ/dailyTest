509db1acdb9bb9075254dc79dc1e0061c399483b
吕金钊 <lvjinzhao@wesai.com>
2018-01-27 20:18:38 +0800

    pjt_10

--- userAuth/utils/email_send.py
+++ userAuth/utils/email_send.py
@@ -1,4 +1,5 @@
 #coding=utf-8
+import traceback
 from random import Random
 from django.core.mail import send_mail
 from userAuth.models import EmailVerifyRecord,User
@@ -16,27 +17,30 @@ def random_str(randomlength=8):
 
 
 def send_register_email(email, send_type="register"):
-    email_record = EmailVerifyRecord()
-    # 将给用户发的信息保存在数据库中
-    code = random_str(16)
-    email_record.code = code
-    email_record.email = email
-    email_record.send_type = send_type
-    email_record.save()
+    try:
+        email_record = EmailVerifyRecord()
+        # 将给用户发的信息保存在数据库中
+        code = random_str(16)
+        email_record.code = code
+        email_record.email = email
+        email_record.send_type = send_type
+        email_record.save()
 
-    # 如果为注册类型
-    if send_type == "register":
-        email_title = "注册激活链接"
-        email_body = "请点击下面的链接激活你的账号:http://127.0.0.1:8000/active/{0}".format(code)
-        # 发送邮件
-        send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
-        if send_status:
-            pass
-    elif send_type == "forget":
-        user=User.objects.get(email=email)
-        email_title = "get pwd"
-        email_body = ("pwd:"+user.password).format(code)
-        # 发送邮件
-        send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
-        if send_status:
-            pass
+        # 如果为注册类型
+        if send_type == "register":
+            email_title = "注册激活链接"
+            email_body = "请点击下面的链接激活你的账号:http://127.0.0.1:8000/active/{0}".format(code)
+            # 发送邮件
+            send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
+            if send_status:
+                pass
+        elif send_type == "forget":
+            user=User.objects.get(email=email)
+            email_title = "get pwd"
+            email_body = ("pwd:"+user.password).format(code)
+            # 发送邮件
+            send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
+            if send_status:
+                pass
+    except Exception, e:
+        traceback.format_exc()
